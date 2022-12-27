83fb683bf78cc7b92ec09ff398c19a7f69483354
Pete <petehorn6@gmail.com>
2022-12-27 20:34:18 +0000

    laser beams working

--- space_invasion/space_invaders.py
+++ space_invasion/space_invaders.py
@@ -8,20 +8,27 @@ HEIGHT=768
 BACKGROUND_IMG = 'space.png'
 
 def draw():
-    global baddies
     screen.blit(BACKGROUND_IMG, (0,0))
     for baddie in baddies:
         baddie.draw()
     goody.draw()
+    for laser in lasers:
+        lb = laser['laser']
+        lb.draw()
 
 def update():
     global direction, timer
     timer += 1
     if timer % 30 == 0:
         direction = move_baddies()
+    for laser in lasers:
+        lb = laser['laser']
+        ep = laser['ep']
+        if lb.x == ep[0] and lb.y == ep[1]:
+            lasers.remove(laser)
 
 def move_baddies():
-    global baddies, direction, move_down
+    global direction, move_down
     movestep = baddie_spacing / 2
     move_dir = direction
     if move_down:
@@ -52,14 +59,43 @@ def new_spaceship(x, y, type):
 def on_mouse_move(pos):
     goody.angle = goody.angle_to(pos) - 90
 
-def on_mouse_down():
+def on_mouse_down(pos):
+    global ss_loc
     laser = Actor('laser')
-    laser.x = goody.x
-    laser.y = goody.y
+    laser.x = ss_loc['x']
+    laser.y = ss_loc['y']
+    laser.angle = goody.angle + 90
+    endpoint = calculate_laser_endpoint(pos)
+    lasers.append({
+        'laser': laser,
+        'ep': list(endpoint)
+        })
+    animate(laser, pos=endpoint)
 
+
+def calculate_laser_endpoint(mouse_pos):
+    global ss_loc
+    ss_x = ss_loc['x']
+    ss_y = ss_loc['y']
+    mp_x = mouse_pos[0]
+    mp_y = mouse_pos[1]
+    x_diff = mp_x - ss_x
+    y_diff = mp_y - ss_y
+    scale = remove_negative(WIDTH / x_diff)
+    endpoint = {
+        x_diff * scale + ss_loc['x'],
+        y_diff * scale + ss_loc['y']
+    }
+    return endpoint
+
+def remove_negative(number):
+    sq = number * number
+    sqrt = math.sqrt(sq)
+    return sqrt
 
 timer = 0
 baddies =[]
+lasers = []
 baddie_offset = 100
 baddie_spacing = 70
 direction = 'r'
@@ -71,5 +107,9 @@ for x in range(5):
                 baddie_offset + x * baddie_spacing, 
                 baddie_offset + y * baddie_spacing,
                 'christmas_tree'))
-goody = new_spaceship(WIDTH / 2, HEIGHT - 50, 'christmas_tree_farm')
+ss_loc = {
+    'x': WIDTH / 2,
+    'y': HEIGHT - 50
+}
+goody = new_spaceship(ss_loc['x'], ss_loc['y'], 'christmas_tree_farm')
 pgzrun.go() # Must be last line
